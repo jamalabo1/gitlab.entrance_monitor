86328eb479084b8ea4adc2ecc566fa174dcdd5dd
Jamal Abo Mokh <jamalabomokh490@gmail.com>
2022-10-10 13:00:10 +0000

    object detector debug k8s

--- src/core/standalone.cpp
+++ src/core/standalone.cpp
@@ -60,46 +60,47 @@ int core::run_services(shared_ptr<core::IoContext> io_context, const std::vector
         post([&post, &io_context, service]() {
             BOOST_LOG_TRIVIAL(debug) << "setting up service: " << typeid(service).name();
             // setup service to register tasks.
-             service->setup();
+            service->setup();
 
             // get registered tasks.
             auto tasks = service->getTasks();
 
             BOOST_LOG_TRIVIAL(debug) << "looping over service tasks";
             // loop over tasks
-            /*
             for (const auto &task: tasks) {
 
                 // post each task to the io_context.
                 post([&io_context, task]() {
                     auto task_name = typeid(*task).name();
-                    // subscribe sub-token to the main token.
-                    auto token = make_shared<CancellationToken>();
+                    /*
 
-                    BOOST_LOG_TRIVIAL(debug) << "configuring task: " << task_name;
-                    // configure the task.
-                    bool result = task->configure();
+        // subscribe sub-token to the main token.
+        auto token = make_shared<CancellationToken>();
 
-                    if(!result) {
-                        // TODO: handle the failing of the task (e.g., re-schedule executing (retry-policy)).
-                    }
+        BOOST_LOG_TRIVIAL(debug) << "configuring task: " << task_name;
+        // configure the task.
+        bool result = task->configure();
 
-                    BOOST_LOG_TRIVIAL(debug) << "setting-up task: " << task_name;
-                    // get runtime options for the task.
-                    auto options = task->setup(io_context, token);
+        if(!result) {
+            // TODO: handle the failing of the task (e.g., re-schedule executing (retry-policy)).
+        }
 
-                    BOOST_LOG_TRIVIAL(debug) << "executing task: " << task_name;
-                    // execute the task, this call must be blocking or non-dependent on options
-                    // because they are freed after the execution complete.
-                    // only `task` lives.
-                    options.executor([task] {
-                            task->operator()();
-                    });
+        BOOST_LOG_TRIVIAL(debug) << "setting-up task: " << task_name;
+        // get runtime options for the task.
+        auto options = task->setup(io_context, token);
+
+        BOOST_LOG_TRIVIAL(debug) << "executing task: " << task_name;
+        // execute the task, this call must be blocking or non-dependent on options
+        // because they are freed after the execution complete.
+        // only `task` lives.
+        options.executor([task] {
+                task->operator()();
+        });
+*/
 
                     BOOST_LOG_TRIVIAL(trace)<< "execution of task: " << task_name  << " has completed";
                 });
             }
-      */
         });
     }
 
