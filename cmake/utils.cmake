930e55ae2c0b9d7b362e024956965faf7fd79e69
Jamal Abo Mokh <jamalabomokh490@gmail.com>
2022-10-13 15:39:25 +0000

    fix missing task registration on stream_obtainer

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -2,32 +2,6 @@ function(install_project_binaries target)
     #    install_amqpclient_binaries(${target})
 endfunction()
 
-macro(expose_target _target_prefix target_prefix)
-    #    string(REPLACE "_" "-" output ${target_prefix})
-    #    message(STATUS ${target_prefix})
-
-    set(_TARGET_LIB "${_target_prefix}_lib")
-    set(_TARGET_STANDALONE "${_target_prefix}_standalone")
-
-    set(TARGET_LIB "${target_prefix}-lib")
-    set(TARGET_STANDALONE "${target_prefix}-standalone")
-
-    #    add_custom_target(
-    #            ${TARGET_LIB}
-    #    )
-    #    target_link_libraries(
-    #            ${TARGET_LIB}
-    #            ${_TARGET_LIB}
-    #    )
-
-    add_library(${TARGET_LIB} ALIAS ${_TARGET_LIB})
-    add_executable(${TARGET_STANDALONE} ALIAS ${_TARGET_STANDALONE})
-endmacro()
-
-macro(expose_testable_target target)
-
-endmacro()
-
 # a macro for creating a test target, link a supplied target to a executable with gtest, and link the target to test (core_tests target wants to test core target)
 # this implies that a library target staffing ${test_target_prefix}_tests exists.
 macro(create_testing_target test_target_prefix)
@@ -77,12 +51,6 @@ macro(enable_global_testing_target)
     set(target_name_exc "${test_target}_exc")
 
     create_testing_executable(${test_target} ${target_name_exc})
-
-#    add_executable(${exc_target_name} $<TARGET_OBJECTS:${test_target}>) # add a executable with test_target objects.
-#
-#    target_link_libraries(${exc_target_name} ${test_target} GTest::gtest) # link with gtest_main for a main entrypoint provided by gtest.
-#
-#    gtest_discover_tests(${exc_target_name}) # integration with CTest
 endmacro()
 
 macro(copy_required_file target file)
@@ -91,5 +59,20 @@ macro(copy_required_file target file)
             COMMAND ${CMAKE_COMMAND} -E copy
             "${PROJECT_SOURCE_DIR}/${file}"
             $<TARGET_FILE_DIR:${target}>
+    )
+endmacro()
+
+macro(copy_local_file target file)
+    if(PROJECT_IS_LOCAL)
+        copy_required_file(${target} ${file})
+    endif()
+endmacro()
+
+macro(set_private_testable_include dirs)
+    set_property(
+            TARGET
+            core
+            APPEND PROPERTY private_include_directories
+            ${dirs}
     )
 endmacro()
